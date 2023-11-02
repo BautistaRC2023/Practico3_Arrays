4b63d403617605c9e8126521b4e7466542c307ee
Bautista <baugamer77@gmail.com>
2023-11-02 02:12:32 -0300

    ultimo comit de noche 2/11

--- javascript/ejercicio1.js
+++ javascript/ejercicio1.js
@@ -2,7 +2,7 @@
 let meses = ['Enero','Febrero', 'Marzo','Abril','Mayo', 'Junio','Julio', 'Agosto','Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 document.write('<h2>Lista de meses</h2>');
 document.write('<ul>');
-for(let i=0; i<ul meses.length; i++){
+for(let i=0; i < meses.length; i++){
    document.write('<li> ${meses[i]}</li>');
 }
  document.write('</ul>')
