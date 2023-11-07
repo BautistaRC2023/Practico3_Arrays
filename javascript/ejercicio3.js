4cba3f96feab611dc909cb815dcb86d779d7418b
Bautista <baugamer77@gmail.com>
2023-11-06 23:23:02 -0300

    ej3

---
+++ javascript/ejercicio3.js
@@ -0,0 +1,26 @@
+/*Escribir un script que simule el lanzamiento de dos dados. 
+Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. 
+Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, 
+repitiendo 50 veces esta operación.*/
+
+const apariciones =[ ];
+
+for(let i = 0 ; i<=12; i++){
+    apariciones.push(0);
+}
+for(let indicedeaparicion = 0 ; indicedeaparicion <50 ; indicedeaparicion++){
+let numerorandom1 = math.floor(math.random() * 6) + 1;
+let numerorandom2 = math.floor(math.random() * 6) + 1;
+let sumanumerosrandom = numerorandom1 + numerorandom2;
+
+apariciones[sumanumerosrandom]++;
+
+
+}
+document.write('<table>');
+document.write('<tr><th>Numero</th><th>apariciones</th></tr>');
+
+
+
+
+
